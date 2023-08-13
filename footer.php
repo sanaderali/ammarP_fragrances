c2d9c7202ce626649b3346df3384a90bab4cc152
sanader <sanaderali007@gmail.com>
2023-08-13 18:22:16 +0500

    ammaer inventory mangemen

---
+++ footer.php
@@ -0,0 +1,56 @@
+<footer>
+        <div class="footer-content">
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-sm-6">
+                <p class="mb-0 text-muted text-medium">Design and develop by <a href="https://techsolutionspro.co.uk">Tech Solutions Pro</a></p>
+              </div>
+              <div class="col-sm-6 d-none d-sm-block">
+                <ul class="breadcrumb pt-0 pe-0 mb-0 float-end">
+                  <li class="breadcrumb-item mb-0 text-medium">
+                    <a href="https://1.envato.market/BX5oGy" target="_blank" class="btn-link">Review</a>
+                  </li>
+                  <li class="breadcrumb-item mb-0 text-medium">
+                    <a href="https://1.envato.market/BX5oGy" target="_blank" class="btn-link">Purchase</a>
+                  </li>
+                  <li class="breadcrumb-item mb-0 text-medium">
+                    <a href="https://acorn-html-docs.coloredstrategies.com/" target="_blank" class="btn-link">Docs</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer> 
+ <!-- Vendor Scripts Start -->
+ <script src="js/vendor/jquery-3.5.1.min.js"></script>
+    <script src="js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="js/vendor/OverlayScrollbars.min.js"></script>
+    <script src="js/vendor/autoComplete.min.js"></script>
+    <script src="js/vendor/clamp.min.js"></script>
+
+    <script src="icon/acorn-icons.js"></script>
+    <script src="icon/acorn-icons-interface.js"></script>
+
+    <script src="js/vendor/jquery.validate/jquery.validate.min.js"></script>
+
+    <script src="js/vendor/jquery.validate/additional-methods.min.js"></script>
+
+    <!-- Vendor Scripts End -->
+
+    <!-- Template Base Scripts Start -->
+    <script src="js/base/helpers.js"></script>
+    <script src="js/base/globals.js"></script>
+    <script src="js/base/nav.js"></script>
+    <script src="js/base/search.js"></script>
+    <script src="js/base/settings.js"></script>
+    <!-- Template Base Scripts End -->
+    <!-- Page Specific Scripts Start -->
+
+    <script src="js/pages/auth.login.js"></script>
+
+    <script src="js/common.js"></script>
+    <script src="js/scripts.js"></script>
+    <!-- Page Specific Scripts End -->
+  </body>
+</html>
